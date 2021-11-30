612f35abda41e39dd6a5d5a5e1af75817e8c9a25
20191003533 <81615887+20191003533@users.noreply.github.com>
2021-11-30 22:55:58 +0800

    Update NewTest.java

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -8,31 +8,26 @@ public class NewTest {
     @Test
     public void testAdd() {
         calcuate = new Calculate();
-        int result = calcuate.add(2, 3);
-        Assert.assertEquals(result,5);
-        /*
-         * "加法有问题"：期望值和实际值不一致时，显示的信息
-         * 5 ：期望值
-         * result ：实际值
-         */
+        int result = calcuate.add(1, 2);
+        Assert.assertEquals(result,3);
 
     }
     @Test
     public void testSubtract() {
         calcuate = new Calculate();
-        int result = calcuate.subtract(12, 2);
-        Assert.assertEquals(result,10); //故意设置减法期望值为10000
+        int result = calcuate.subtract(5, 2);
+        Assert.assertEquals(result,3); //故意设置减法期望值为10000
     }
     @Test
     public void testMultiply() {
         calcuate = new Calculate();
-        int result = calcuate.multiply(2, 3);
-        Assert.assertEquals(result, 6);
+        int result = calcuate.multiply(4, 3);
+        Assert.assertEquals(result, 12);
     }
     @Test
     public void testDivide() {
         calcuate = new Calculate();
-        int result = calcuate.divide(6, 3);
-        Assert.assertEquals(result, 2);
+        int result = calcuate.divide(9, 3);
+        Assert.assertEquals(result, 3);
     }
 }
